5840b17615002ad2b75d6912a6a1deea8c77bb38
calmBlooper <pifagor6541@gmail.com>
2019-12-01 19:02:59 +0200

    Base

---
+++ public/chat.js
@@ -0,0 +1,29 @@
+$(function(){
+	//make connection
+	var socket=io.connect('http://localhost:3000')
+
+//buttons and inputs
+var message= $("#message-written")
+var username= $("#username")
+var send_message =$("#send")
+var send_username =$("#enter")
+var chatroom = $("#messages-container")
+
+//Emit message 
+send_message.click(function(){
+	socket.emit('new_message',{message : message.val()})
+})
+
+//Listen on new message
+socket.on("new_message", (data)=>{
+	console.log(data)
+	chatroom.append("<p class='message'>"+data.username+": "+data.message+"</p")
+})
+//Emit a username
+send_username.click(function(){
+	console.log(username.val())
+	if (username.val()==="dick") $(".delete-b").text("fuck it");
+	socket.emit('change_username', {username : username.val()})
+})
+
+});
