21a891398519f6f14dba5ee34f358f52129dfc48
calmBlooper <pifagor6541@gmail.com>
2019-12-02 01:35:06 +0200

    Cooool

--- public/chat.js
+++ public/chat.js
@@ -16,20 +16,29 @@ send_message.click(function(){
 
 //Listen on new message
 socket.on("new_message", (data)=>{
-	console.log(data)
+	//console.log(data)
 	var today = new Date();
+	var ss=today.getSeconds();
+	var mns=today.getMinutes();
+	var hrs=today.getHours();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
 today = String(mm + '/' + dd + '/' + yyyy);
 //document.write(today);
-	chatroom.append("<p class='message'>"+data.username+mm +today +": "+data.message+"</p")
+ let itm= document.getElementsByClassName("message")[0];
+let cartItem=itm.cloneNode(true);
+cartItem.style.display="block";
+cartItem.getElementsByClassName("text")[0].innerHTML="<font color=#117743>"+data.username+"</font> at "+hrs+":"+mns+":"+ss+" "+dd +"/"+mm+"/"+yyyy +" wrote:"+"<p style=\"word-wrap: break-word;\">"+data.message+"</p";
+//console.log(cartItem);
+document.getElementById("messages-container").appendChild(cartItem);
+	//chatroom.append("<p class='message'>"+cartItem+data.username+dd +"/"+mm+"/"+yyyy +": "+data.message+"</p")
 })
 //Emit a username
 send_username.click(function(){
 	console.log(username.val())
-	if (username.val()==="dick") $(".delete-b").text("fuck it");
+	if (username.val()==="dick") $(".delete-b").text("DELETE");
 	socket.emit('change_username', {username : username.val()})
 })
 
